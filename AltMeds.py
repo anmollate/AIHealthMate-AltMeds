07f989d641916aa36dca48cd008617985a1c0cc1
Anmol Late <138355891+anmollate@users.noreply.github.com>
2025-07-28 22:53:10 +0530

    AltMeds Structure WIP

--- AltMeds.py
+++ AltMeds.py
@@ -1,25 +1,25 @@
 import pandas as pd
+import streamlit as st
 
 df=pd.read_excel('Medicine Dataset.xlsx')
 
-MedName=input("Enter The Name Of Medicine: ")
+st.subheader("Search Medicines🔍💊")
+MedName=st.text_input("Enter The Name Of Medicine: ")
 
 row_matched=df[df['Product Name'].str.lower()==MedName.lower()]
 
 if not row_matched.empty:
     salt_composition=row_matched.iloc[0]['salt_composition']
-    print("Contents: ",salt_composition)
+    st.write("Contents: ",salt_composition)
     usage=row_matched.iloc[0]['primary_use']
-    print("Primary Usage: ",usage)
+    st.write("Primary Usage: ",usage)
 
     similar_meds=df[df['salt_composition'].str.lower()==salt_composition.lower()]
 
     if not similar_meds.empty and len(similar_meds)!=1:
         for _,row in similar_meds.iterrows():
-            print("Alternate Medicines: ",row['Product Name'],"     Contents: ",row['salt_composition'])
+            st.write("Alternate Medicines: ",row['Product Name'],"     Contents: ",row['salt_composition'])
     else:
-        print("No Alternatives Availabel At The Moment !")
-
-
+        st.write("No Alternatives Availabel At The Moment !")
 else:
-    print("Med Not Found!")
+    st.write("Med Not Found!")
